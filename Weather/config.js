da7096cd9dd214000f278ee823549b9a4ef5c2e6
Soumalya Khan <toast.maketext@gmail.com>
2022-03-25 05:51:38 +0530

    initial commit

--- Weather/config.js
+++ Weather/config.js
@@ -1,8 +1,6 @@
-var config = {
+export var config = {
     MY_API_TOKEN : '48f37cf1f422925a49866d316756a826',
     SECRET_API_KEY : 'kk&jk',
   }
 
-  export function hello() {
-    return config;
-  }
+  
--- Weather/weather.js
+++ Weather/weather.js
@@ -1,7 +1,8 @@
 //let response=fetch(api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
+import {config} from "./config.js"
 
-let api_key='48f37cf1f422925a49866d316756a826';
+let api_key=config.MY_API_TOKEN;
 let url='https://api.openweathermap.org/data/2.5/';
 let txtBox=document.getElementById('sBox');
 txtBox.addEventListener('keypress',display);
@@ -60,4 +61,6 @@ let str=dt.toLocaleString("en-us", options);
 date.innerText=str;
 
 }
+
+window.onload=dateFunction
 
